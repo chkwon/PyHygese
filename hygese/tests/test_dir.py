426da17aee8b0b1ed2613679e2104d8d9c016cba
Changhyun Kwon <chkwon@gmail.com>
2022-03-18 13:52:25 +0900

    Update test_dir.py

--- hygese/tests/test_dir.py
+++ hygese/tests/test_dir.py
@@ -2,6 +2,10 @@ import hygese, subprocess, os
 
 def test_dir():
     print("Hello Dir!")
-    print(hygese.HGS_LIBRARY_FILEPATH)
+    print("hygese.HGS_LIBRARY_FILEPATH = ", hygese.HGS_LIBRARY_FILEPATH)
     subprocess.check_call('ls')
     os.listdir() 
+    os.listdir("hygese")
+
+    a = 10
+    assert a == 10
