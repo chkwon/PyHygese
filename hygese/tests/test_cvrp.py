eb16e2ff1bf4b771deb42f74299bcfbc34c4766d
Changhyun Kwon <chkwon@gmail.com>
2022-03-18 13:22:29 +0900

    test files

--- hygese/tests/test_cvrp.py
+++ hygese/tests/test_cvrp.py
@@ -1,39 +1,7 @@
 import numpy as np 
 from hygese import AlgorithmParameters, Solver
 
-def cvrp_example(n):
-    x = (np.random.rand(n) * 1000)
-    y = (np.random.rand(n) * 1000)
-    verbose = True
-
-    # Solver initialization
-    ap = AlgorithmParameters()
-    ap.timeLimit = 3.2
-    hgs_solver = Solver(ap, verbose)
-
-    # data preparation
-    data = {}
-    data['x_coordinates'] = x
-    data['y_coordinates'] = y
-
-    # You may also supply distance_matrix instead of coordinates, or in addition to coordinates
-    # If you supply distance_matrix, it will be used for cost calculation.
-    # The additional coordinates will be helpful in speeding up the algorithm.
-    # data['distance_matrix'] = dist_mtx
-
-    data['service_time'] = np.zeros(n)
-    demand = np.ones(n)
-    demand[0] = 0 # depot demand = 0
-    data['demands'] = demand
-    data['vehicle_capacity'] = np.ceil(n/3).astype(int)
-    data['num_vehicles'] = 3
-    data['depot'] = 0
-
-    result = hgs_solver.solve_cvrp(data)
-    print(result.cost)
-    print(result.routes)
-
-def cvrp_or_tools():
+def test_cvrp():
     data = {}
     data['distance_matrix'] = [
         [
@@ -124,7 +92,3 @@ def cvrp_or_tools():
     assert(result.cost == 6208)
 
 
-if __name__ == '__main__':
-    n = 20
-    cvrp_example(n)
-    cvrp_or_tools()
--- hygese/tests/test_tsp.py
+++ hygese/tests/test_tsp.py
@@ -1,81 +1,8 @@
 import numpy as np 
 from hygese import AlgorithmParameters, Solver
 
-def tsp_by_coordinates(n):
-    x = (np.random.rand(n) * 1000)
-    y = (np.random.rand(n) * 1000)
-    verbose = False
 
-    # data preparation
-    data = {}
-    data['x_coordinates'] = x
-    data['y_coordinates'] = y
-
-    # Solver initialization
-    ap = AlgorithmParameters()
-    ap.timeLimit = 1.3 # seconds
-    hgs_solver = Solver(ap, verbose)
-
-    # solve TSP using coordinates only
-    result = hgs_solver.solve_tsp(data)
-    print(result.cost)
-    print(result.routes)
-
-def tsp_by_dist_mtx(n):
-    x = (np.random.rand(n) * 1000)
-    y = (np.random.rand(n) * 1000)
-    verbose = False
-
-    # data preparation
-    data = {}
-    dist_mtx = np.round(np.random.rand(n, n) * 1000)
-    for i in range(n):
-        for j in range(n):
-            dist_mtx[i][j] = np.math.sqrt(
-                (x[i] - x[j]) ** 2 + (y[i] - y[j]) ** 2
-            )
-    data['distance_matrix'] = dist_mtx
-
-    # Solver initialization
-    ap = AlgorithmParameters()
-    ap.timeLimit = 1.3 # seconds
-
-    # solve TSP using by dist_mtx only
-    hgs_solver = Solver(ap, verbose)
-    result = hgs_solver.solve_tsp(data)
-    print(result.cost)
-    print(result.routes)
-
-
-def tsp_by_both(n):
-    x = (np.random.rand(n) * 1000)
-    y = (np.random.rand(n) * 1000)
-    verbose = False
-
-    # data preparation
-    data = {}
-    data['x_coordinates'] = x
-    data['y_coordinates'] = y
-
-    dist_mtx = np.round(np.random.rand(n, n) * 1000)
-    for i in range(n):
-        for j in range(n):
-            dist_mtx[i][j] = np.math.sqrt(
-                (x[i] - x[j]) ** 2 + (y[i] - y[j]) ** 2
-            )
-    data['distance_matrix'] = dist_mtx
-
-    # Solver initialization
-    ap = AlgorithmParameters()
-    ap.timeLimit = 1.3 # seconds
-
-    # solve TSP using both coordinates and dist_mtx
-    hgs_solver = Solver(ap, verbose)
-    result = hgs_solver.solve_tsp(data)
-    print(result.cost)
-    print(result.routes)
-
-def tsp_ortools():
+def test_tsp():
     data = {}
     data['distance_matrix'] = [
         [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
@@ -105,9 +32,3 @@ def tsp_ortools():
     
     assert(result.cost == 7293)
 
-if __name__ == '__main__':
-    n = 20
-    tsp_by_coordinates(n)
-    tsp_by_dist_mtx(n)
-    tsp_by_both(n)
-    tsp_ortools()
