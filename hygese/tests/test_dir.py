efc0513a7a56f1f3f843227c1d2f99c61f94b367
Changhyun Kwon <chkwon@gmail.com>
2022-03-18 13:48:56 +0900

    Update test_dir.py

--- hygese/tests/test_dir.py
+++ hygese/tests/test_dir.py
@@ -1,6 +1,7 @@
-import hygese, subprocess
+import hygese, subprocess, os
 
 def test_dir():
     print("Hello Dir!")
     print(hygese.HGS_LIBRARY_FILEPATH)
     subprocess.check_call('ls')
+    os.listdir() 
