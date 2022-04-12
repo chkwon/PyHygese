d293742c5e37d7b224cde74a742b2ce826e266bc
Changhyun Kwon <chkwon@gmail.com>
2022-04-12 15:43:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,9 +35,13 @@ def _safe_makedirs(*paths):
 HGS_VERSION = "f40c0a465f0df99db3e17c89bf8d9f2f3f0f383a"
 HGS_SRC = f"https://github.com/chkwon/HGS-CVRP/archive/{HGS_VERSION}.tar.gz"
 
-HGS_CVRP_URL = f"https://github.com/chkwon/HGS_CVRP_jll.jl/releases/download/HGS_CVRP-v{HGS_VERSION}%2B0"
-HGS_CVRP_WIN = f"{HGS_CVRP_URL}/libhgscvrp.v{HGS_VERSION}.x86_64-w64-mingw32-cxx11.tar.gz"
-HGS_CVRP_WIN = "https://github.com/chkwon/HGS-CVRP/suites/6085969291/artifacts/210833023"
+
+LIB_VERSION = "0.1.0"
+HGS_CVRP_URL = f"https://github.com/chkwon/Libhgscvrp_jll.jl/releases/download/libhgscvrp-v{LIB_VERSION}%2B0"
+HGS_CVRP_WIN = f"{HGS_CVRP_URL}/libhgscvrp.v{LIB_VERSION}.x86_64-w64-mingw32-cxx11.tar.gz"
+# https://github.com/chkwon/Libhgscvrp_jll.jl/releases/download/libhgscvrp-v0.0.1%2B0/libhgscvrp.v0.0.1.x86_64-w64-mingw32-cxx11.tar.gz
+
+
 
 LIB_DIR = "lib"
 BUILD_DIR = "lib/build"
